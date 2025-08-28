ace9623c6fb52f10534c045ea1d80bd4caee23a5
janelleopj <janelleopj@gmail.com>
2025-08-28 13:58:06 +0800

    edit calibrate

--- scripts/kexin/leader_calibrate.py
+++ scripts/kexin/leader_calibrate.py
@@ -1,11 +1,22 @@
 from lerobot.robots.so101_follower import SO101FollowerConfig, SO101Follower
+from lerobot.teleoperators.so101_leader import SO101LeaderConfig, SO101Leader
 
-config = SO101FollowerConfig(
-    port="/dev/tty.usbmodem585A0076891",
-    id="my_awesome_follower_arm",
+follower_config = SO101FollowerConfig(
+    port="/dev/ttyACM0",
+    id="follow_arm",
 )
 
-follower = SO101Follower(config)
+leader_config = SO101LeaderConfig(
+    port="/dev/ttyACM1",
+    id="leader_arm",
+)
+
+follower = SO101Follower(follower_config)
 follower.connect(calibrate=False)
 follower.calibrate()
 follower.disconnect()
+
+leader = SO101Leader(leader_config)
+leader.connect(calibrate=False)
+leader.calibrate()
+leader.disconnect()
